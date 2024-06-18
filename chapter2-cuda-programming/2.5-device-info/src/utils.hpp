f90c66a916d7ab877bfe97772c6b91b50b334ae4
Mauerrr <chenzehao618@gmail.com>
2024-06-18 11:34:35 +0800

    gh

--- chapter2-cuda-programming/2.5-device-info/src/utils.hpp
+++ chapter2-cuda-programming/2.5-device-info/src/utils.hpp
@@ -5,6 +5,11 @@
 #include <system_error>
 #include <stdarg.h>
 
+//在C/C++中，#define用于定义宏，它是预处理指令的一部分。宏本质上是一种文本替换工具，可以用来在编译之前替换代码中的文本。定义了一个名为CUDA_CHECK的宏。这个宏接收一个参数call，并在代码中扩展为调用函数__cudaCheck，同时传递三个参数：call、__FILE__和__LINE__。CUDA_CHECK是宏的名称，call是它的参数，这个参数在宏被使用时将被替换成具体的CUDA API调用。
+//在C和C++中，名称前面加双下划线（__）或者以一个下划线后跟一个大写字母开始（_X）的做法通常是保留给编译器和标准库的实现的。
+//避免命名冲突：使用这种命名约定可以避免与用户程序中的普通函数或变量名发生冲突。
+//标明内部或低级功能：以双下划线开头的函数通常表示它们是低级或仅供内部使用的功能，普通开发者在日常编程中应避免直接调用这些函数。
+//强调特殊用途：这种命名风格也用来标明某些特殊的、具有特定用途的函数或变量，如宏定义中使用的函数。
 #define CUDA_CHECK(call)             __cudaCheck(call, __FILE__, __LINE__)
 #define LAST_KERNEL_CHECK(call)      __kernelCheck(__FILE__, __LINE__)
 #define LOG(...)                     __log_info(__VA_ARGS__)
--- chapter2-cuda-programming/2.7-matmul-shared-memory/src/matmul_gpu_shared.cu
+++ chapter2-cuda-programming/2.7-matmul-shared-memory/src/matmul_gpu_shared.cu
@@ -5,24 +5,34 @@
 
 /* 
     使用shared memory把计算一个tile所需要的数据分块存储到访问速度快的memory中
+    M_device: 指向设备（GPU）上第一个矩阵的指针。
+    N_device: 指向设备（GPU）上第二个矩阵的指针。
+    P_device: 指向设备（GPU）上结果矩阵的指针。
+    width: 矩阵的宽度（假设是方阵，即行数和列数相同）。
 */
 __global__ void MatmulSharedStaticKernel(float *M_device, float *N_device, float *P_device, int width){
+    //声明共享内存
     __shared__ float M_deviceShared[BLOCKSIZE][BLOCKSIZE];
     __shared__ float N_deviceShared[BLOCKSIZE][BLOCKSIZE];
     /* 
         对于x和y, 根据blockID, tile大小和threadID进行索引
     */
+    //计算线程的索引，threadIdx.x和threadIdx.y是线程在block里的下x和y的索引，是一个二维块
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     float P_element = 0.0;
+    //这里的ty和tx是为了方便后面的计算，类似于简化书写
+    //ty和tx对于每个线程都是不一样的，每个线程在执行核函数时都会运行相同的代码，由于 threadIdx.x 和 threadIdx.y 不同，每个线程将从全局内存中的不同位置加载数据到共享内存中的不同位置。因为所有线程是并行执行的，所以整个矩阵乘法的计算会非常高效。
 
     int ty = threadIdx.y;
     int tx = threadIdx.x;
-    /* 对于每一个P的元素，我们只需要循环遍历width / tile_width 次就okay了，这里有点绕，画图理解一下*/
+    /* 对于每一个P的元素，我们只需要循环遍历width / tile_width 次就okay了，这里有点绕，画图理解一下。tile是子块*/
     for (int m = 0; m < width / BLOCKSIZE; m ++) {
+        //从global memory中写取数据到shared memory中，y*width代表当前线程所在的起始位置在全局矩阵中的索引，m代表第几个子块
         M_deviceShared[ty][tx] = M_device[y * width + (m * BLOCKSIZE + tx)];
         N_deviceShared[ty][tx] = N_device[(m * BLOCKSIZE + ty)* width + x];
+        //与shared memory中的线程进行一个同步
         __syncthreads();
 
         for (int k = 0; k < BLOCKSIZE; k ++) {
@@ -31,6 +41,7 @@ __global__ void MatmulSharedStaticKernel(float *M_device, float *N_device, float
         __syncthreads();
     }
 
+    //最后通过把每个线程的计算结果写入到全局内存中，因为他们写不同的位置，所以不会冲突
     P_device[y * width + x] = P_element;
 }
 
@@ -41,10 +52,12 @@ __global__ void MatmulSharedDynamicKernel(float *M_device, float *N_device, floa
             __shared__ float M_deviceShared[];
             __shared__ float N_deviceShared[];
         因为在cuda中定义动态共享变量的话，无论定义多少个他们的地址都是一样的。
+        所以直接声明一个动态空间，然后用不同的指针来搞
         所以如果想要像上面这样使用的话，需要用两个指针分别指向shared memory的不同位置才行
     */
 
     extern __shared__ float deviceShared[];
+    //stride相当于一个偏移
     int stride = blockSize * blockSize;
     /* 
         对于x和y, 根据blockID, tile大小和threadID进行索引
